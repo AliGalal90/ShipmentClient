4972572e74ad062d8af4def56356a9a43cbabd5d
Ali Galal <ali.galal9011@gmail.com>
2018-07-09 00:13:24 +0200

    populate shipment client

--- ShipmentClient/Program.cs
+++ ShipmentClient/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace ShipmentClient
+﻿using CourierNumberFour;
+using CourierNumberOne;
+using CourierNumberThree;
+using CourierNumberTwo;
+using System;
+
+namespace ShipmentClient
 {
     internal class Program
     {
+        private static CourierClient courierClient;
+
         private static void Main(string[] args)
         {
+            courierClient = new CourierClient(new CourierNumberOneApiWrapper());
+            ProcessShipment(courierClient);
 
+            courierClient = new CourierClient(new CourierNumberTwoApiWrapper());
+            ProcessShipment(courierClient);
+
+            courierClient = new CourierClient(new CourierNumberThreeApiWrapper());
+            ProcessShipment(courierClient);
+
+            courierClient = new CourierClient(new CourierNumberFourApiWrapper());
+            ProcessShipment(courierClient);
+
+            Console.ReadKey();
+        }
+
+        private static void ProcessShipment(CourierClient courierClient)
+        {
+            courierClient.CreateShipment();
+            courierClient.TrackShipment();
         }
     }
 }
