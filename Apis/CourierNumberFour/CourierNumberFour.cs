c100d5375186004cc2d89f4899656d9339eada3c
Ali Galal <ali.galal9011@gmail.com>
2018-07-09 00:04:51 +0200

    implement courier number four api

---
+++ Apis/CourierNumberFour/CourierNumberFour.cs
@@ -0,0 +1,30 @@
+﻿using ShipmentCourierInterface;
+using System;
+
+namespace CourierNumberFour
+{
+    public class CourierNumberFourApi
+    {
+        public void CreateShipmentInfo()
+        {
+            Console.WriteLine("Hello from CreateShipmentInfo => Courier number four");
+        }
+        public void CreateShipmentInstructions()
+        {
+            Console.WriteLine("Hello from CreateShipmentInstructions => Courier number four");
+        }
+        public void GetShimpmentWayBill()
+        {
+            Console.WriteLine("Hello from GetShimpmentWayBill => Courier number four");
+        }
+        public void RegisterNumberToGetWayBill()
+        {
+            Console.WriteLine("Hello from RegisterNumberToGetWayBill => Courier number four");
+        }
+        public void GetTrackingDetails()
+        {
+            Console.WriteLine("Hello from GetTrackingDetails => Courier number four");
+            Console.WriteLine("========================================================");
+        }
+    }
+}
