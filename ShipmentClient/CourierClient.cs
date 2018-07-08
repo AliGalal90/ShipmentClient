6339b02f505274aa8c19a083c81b1195290bbd94
Ali Galal <ali.galal9011@gmail.com>
2018-07-09 00:10:52 +0200

    implement courier client

---
+++ ShipmentClient/CourierClient.cs
@@ -0,0 +1,14 @@
+﻿using ShipmentCourierInterface;
+
+namespace ShipmentClient
+{
+    public class CourierClient : IShipmentCourier
+    {
+        private readonly IShipmentCourier _shipmentCourier;
+
+        public CourierClient(IShipmentCourier shipmentCourier) => _shipmentCourier = shipmentCourier;
+
+        public void CreateShipment() => _shipmentCourier.CreateShipment();
+        public void TrackShipment() => _shipmentCourier.TrackShipment();
+    }
+}
