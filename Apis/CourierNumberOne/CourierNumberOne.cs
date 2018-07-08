4047b1a9f9fdb1b5aa1132ab49c69a9278da032f
Ali Galal <ali.galal9011@gmail.com>
2018-07-08 23:57:40 +0200

    implement courier number one api

---
+++ Apis/CourierNumberOne/CourierNumberOne.cs
@@ -0,0 +1,18 @@
+﻿using ShipmentCourierInterface;
+using System;
+
+namespace CourierNumberOne
+{
+    public class CourierNumberOneApi
+    {
+        public void CreateShipmentAndGetWayBill()
+        {
+            Console.WriteLine("Hello from CreateShipmentAndGetWayBill => Courier number one");
+        }
+        public void GetShipmentTrackingDetails()
+        {
+            Console.WriteLine("Hello from GetShipmentTrackingDetails => Courier number one");
+            Console.WriteLine("========================================================");
+        }
+    }
+}
