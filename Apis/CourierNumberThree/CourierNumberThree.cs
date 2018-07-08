b4406df23db949ddf42a088bdadb5949be4aeb1a
Ali Galal <ali.galal9011@gmail.com>
2018-07-09 00:02:09 +0200

    implement courier number three api

---
+++ Apis/CourierNumberThree/CourierNumberThree.cs
@@ -0,0 +1,22 @@
+﻿using ShipmentCourierInterface;
+using System;
+
+namespace CourierNumberThree
+{
+    public class CourierNumberThreeApi
+    {
+        public void CreateShipmentAndGetWayBill()
+        {
+            Console.WriteLine("Hello from CreateShipmentAndGetWayBill => Courier number three");
+        }
+        public void RegisterNumberToGetWayBill()
+        {
+            Console.WriteLine("Hello from RegisterNumberToGetWayBill => Courier number three");
+        }
+        public void GetTrackingDetails()
+        {
+            Console.WriteLine("Hello from GetTrackingDetails => Courier number three");
+            Console.WriteLine("========================================================");
+        }
+    }
+}
