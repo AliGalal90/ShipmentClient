96937d769c0b3236d2cb563cba879d8a37fe51f4
Ali Galal <ali.galal9011@gmail.com>
2018-07-08 23:59:58 +0200

    implement corier number two api

---
+++ Apis/CourierNumberTwo/CourierNumberTwo.cs
@@ -0,0 +1,26 @@
+﻿using ShipmentCourierInterface;
+using System;
+
+namespace CourierNumberTwo
+{
+    public class CourierNumberTwoApi
+    {
+        public void CreateShipmentInfo()
+        {
+            Console.WriteLine("Hello from CreateShipmentInfo => Courier number two");
+        }
+        public void CreateShipmentInstructions()
+        {
+            Console.WriteLine("Hello from CreateShipmentInfo => Courier number two");
+        }
+        public void GetShimpmentWayBill()
+        {
+            Console.WriteLine("Hello from GetShimpmentWayBill => Courier number two");
+        }
+        public void GetShipmentTrackingDetails()
+        {
+            Console.WriteLine("Hello from GetShipmentTrackingDetails => Courier number two");
+            Console.WriteLine("========================================================");
+        }
+    }
+}
